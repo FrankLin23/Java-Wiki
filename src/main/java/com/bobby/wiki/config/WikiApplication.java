3be4856b013088ea23905e79fc000e9c32e335ed
bobby <linzhangsheng23@163.com>
2021-02-25 15:49:04 +0800

    Develop Hello Interface

--- src/main/java/com/bobby/wiki/config/WikiApplication.java
+++ src/main/java/com/bobby/wiki/config/WikiApplication.java
@@ -1,12 +1,14 @@
-package com.bobby.wiki;
+package com.bobby.wiki.config;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 import org.slf4j.Logger;
 
+@ComponentScan("com.bobby")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
