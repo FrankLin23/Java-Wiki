581925021b243deb6229683f36048bab6cefccc8
bobby <linzhangsheng23@163.com>
2021-02-27 14:00:39 +0800

    Add Test File test.http

--- src/main/java/com/bobby/wiki/controller/TestController.java
+++ src/main/java/com/bobby/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.bobby.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class TestController {
     @GetMapping("/hello")
     public String Hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("hello/post")
+    public String HelloPost(String name) {
+        return "Hello World" + name;
     }
 }
