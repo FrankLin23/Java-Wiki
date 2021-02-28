b40e843470ea2820134bea59135a6f5f23861aff
bobby <linzhangsheng23@163.com>
2021-02-28 12:49:15 +0800

    Add Ebook Query Interface.

---
+++ src/main/java/com/bobby/wiki/service/EbookService.java
@@ -0,0 +1,20 @@
+package com.bobby.wiki.service;
+
+import com.bobby.wiki.domain.Ebook;
+import com.bobby.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+
+}
