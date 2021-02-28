c3f0d1c0171ff933af3b6dca62fdfff6750a1fe6
bobby <linzhangsheng23@163.com>
2021-02-28 14:19:47 +0800

    Package request parameters.

--- src/main/java/com/bobby/wiki/controller/EbookController.java
+++ src/main/java/com/bobby/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.bobby.wiki.controller;
 
-import com.bobby.wiki.domain.Ebook;
+import com.bobby.wiki.req.EbookReq;
 import com.bobby.wiki.resp.CommonResp;
+import com.bobby.wiki.resp.EbookResp;
 import com.bobby.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
