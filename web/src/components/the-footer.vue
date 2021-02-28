bd4853292ba4014445347f56900b8ef962a012ce
bobby <linzhangsheng23@163.com>
2021-02-28 16:04:36 +0800

    Add Components.

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Rich Captial @2021 Created By BobbyLin
+  </a-layout-footer>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
+
+<style scoped>
+
+</style>
