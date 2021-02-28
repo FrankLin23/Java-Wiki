0a8764739651c84edf2d7c06d5d6f9537f401d07
bobby <linzhangsheng23@163.com>
2021-02-28 11:42:48 +0800

    Integration Mybatis Generator and Generate Codes.

--- doc/all.sql
+++ doc/all.sql
@@ -1,1 +1,9 @@
 insert into `test` (id, name, password) values (1, '测试', 'password');
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名字',
+    primary key (`id`)
+) engine=innodb default charset = utf8mb4 comment 'demo';
+insert into `demo` (id, name) values (1, '测试');
