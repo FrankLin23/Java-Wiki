69e9188cd00f3c013f43acd4a5aea6e5b6b8e6da
bobby <linzhangsheng23@163.com>
2021-02-28 12:43:38 +0800

    Add Ebook sql and generate ebook codes.

---
+++ src/main/java/com/bobby/wiki/service/DemoService.java
@@ -0,0 +1,20 @@
+package com.bobby.wiki.service;
+
+import com.bobby.wiki.domain.Demo;
+import com.bobby.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+
+}
