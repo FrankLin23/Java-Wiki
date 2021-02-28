5ac4f1328e96271e3d922a1308043cb09fc3ff34
bobby <linzhangsheng23@163.com>
2021-02-28 12:58:48 +0800

    Add CommonResp.java.

--- src/main/java/com/bobby/wiki/controller/EbookController.java
+++ src/main/java/com/bobby/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.bobby.wiki.controller;
 
 import com.bobby.wiki.domain.Ebook;
+import com.bobby.wiki.resp.CommonResp;
 import com.bobby.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
